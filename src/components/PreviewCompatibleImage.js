f6bf96905981ccd07be02d619d4f1ec1e1ec6817
Elliot <elliot.morris@bjss.com>
2021-03-31 22:23:32 +0100

    new img

--- src/components/PreviewCompatibleImage.js
+++ src/components/PreviewCompatibleImage.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Img from 'gatsby-image'
 
 const PreviewCompatibleImage = ({ imageInfo }) => {
-  const imageStyle = { borderRadius: '0px', backgroundColor: '#d8d8d8' }
+  const imageStyle = { borderRadius: '0px'}
   const { alt = '', childImageSharp, image } = imageInfo
 
   if (!!image && !!image.childImageSharp) {
